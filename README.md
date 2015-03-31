b2ae055cb0b48a823cadad15e950a387974fcf35
RefotsirkNged <kristoffer@hylleby.dk>
2015-03-31 11:49:23 +0200

    P2 stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-# p2
-P2
-Repo for our P2 Project, about arranging and running an esports tournament.
---
+++ WindowsFormsApplication9/Tournament.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2.Turnering
+{
+	public class Tournament
+	{
+		String Name;
+		int StartTime;
+		int TeamAmount;
+		String Game;
+		String Format;
+	}
+}
