0f129e5731818f6c5be28bd828d1920780a7f122
SpaceCowboy5001 <spacecowboy5001@gmail.com>
2015-03-31 14:00:43 +0200

    first attempt
    had to restart visual studio, functions went missing.. go figure

--- WindowsFormsApplication9/Form1.cs
+++ WindowsFormsApplication9/Form1.cs
@@ -16,5 +16,25 @@ namespace WindowsFormsApplication9
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
