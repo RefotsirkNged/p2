6e48ca1b735e44e016401216cfe4148ef435c179
angube <daniel.boassen@gmail.com>
2015-04-09 14:27:05 +0200

    Tournament Tree
    WFA with example of Tournament Tree

---
+++ WindowsFormsApplication9/TournamentTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicTest
+{
+    public partial class TournamentTree : Form
+    {
+        public TournamentTree()
+        {
+            InitializeComponent();
+        }
+
+        //Code that creates lines
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            Pen myPen = new Pen(System.Drawing.Color.Red, 3);
+            //Upper line             (x, y) start, (x, y) end
+            graphicsObj.DrawLine(myPen, 20, 40, 170, 40);
+            //Lower line
+            graphicsObj.DrawLine(myPen, 20, 180, 170, 180);
+            //Upper to mid line
+            graphicsObj.DrawLine(myPen, 170, 40, 250, 110);
+            //Lower to mid line
+            graphicsObj.DrawLine(myPen, 170, 180, 250, 110);
+            //End line
+            graphicsObj.DrawLine(myPen, 250, 110, 280, 110);
+
+        }        
+    }
+}
