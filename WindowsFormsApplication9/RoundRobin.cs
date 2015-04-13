c3bcb2e6bb9ed82634e434bdff8265321a4e0a8e
jaadare <jaadare88@gmail.com>
2015-04-13 10:34:24 +0200

    nu med sortlist i single
    fhkjsdfh

--- WindowsFormsApplication9/RoundRobin.cs
+++ WindowsFormsApplication9/RoundRobin.cs
@@ -4,11 +4,7 @@ using System.Text;
 
 namespace P2.Turnering
 {
-	public class RoundRobin
-	{
-		public void MakeTable()
-		{
-			throw new NotImplementedException();
-		}
-	}
+    public class RoundRobin
+    {
+    }
 }
--- WindowsFormsApplication9/SingleElim.cs
+++ WindowsFormsApplication9/SingleElim.cs
@@ -2,9 +2,64 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace P2.Turnering
-{
-	public class SingleElim
-	{
-	}
+namespace WindowsFormsApplication9
+{   public class SingleElimination
+    {
+    //placeholder liste
+   public List<string> hold = new List<string>();
+
+   public void sortList(){
+        hold.Add ("Peter");
+        hold.Add ("Mathias");
+        hold.Add ("Daniel");
+        hold.Add ("Kris");
+        hold.Add ("Jakob");
+        hold.Add ("Rasmus");
+        hold.Add ("Ghost1");
+        hold.Add ("Ghost2");
+
+       int NumTeams  = hold.Count;
+       List<string> sorted = new List<string>();
+    
+       for (int i = 0; i < NumTeams/2; i++)
+			{
+			 sorted.Add(hold[i]);
+             sorted.Add(hold[NumTeams-i]);
+  			}
+}
+	public SingleElimination()
+        {
+       
+        
+
+            int nte = 0;
+            int team = 0; // team er index for hold i ovenstående array / list / collection / whatever
+            int teamsLeft = Convert.ToInt32(Math.Pow(2, nte));
+ 
+            while (teamsLeft != 1)
+            {
+                for (team = 0; team < (teamsLeft / 2); ++team)
+                {
+                    //team vs (teamsLeft - team);
+                }
+ 
+                teamsLeft = teamsLeft / 2;
+            }
+ 
+            //Og vinderen er
+     }
+        public CleanNumberOfTeams()
+        {
+            int nte = 0;
+            int howManyTeams = hold().count;
+ 
+            while (Math.Pow(2, nte) < howManyTeams)
+            {
+                ++nte;
+            }
+            TournamentCount.Text = Convert.ToString(Convert.ToInt32(Math.Pow(2, nte)));
+ 
+            //Something der får alle holdene ind i et array / list / collection / whatever        
+        }
+    }
 }
