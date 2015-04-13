563e76a37e9e16ca953f79a8c35a99717283df84
RefotsirkNged <kristoffer@hylleby.dk>
2015-04-13 10:35:18 +0200

    dsffdfsdfs
    sfdfddsfsd

--- WindowsFormsApplication9/TournamentLists.cs
+++ WindowsFormsApplication9/TournamentLists.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace WindowsFormsApplication9
 {
+    //remainingtemalist needed?
     public class TournamentLists
     {
         private List<List<TeamModel>> _groupList = new List<List<TeamModel>>();
