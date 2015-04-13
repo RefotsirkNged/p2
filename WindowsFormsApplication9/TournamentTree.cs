eb054b4f3e1af160d1d03f8320509c738658eebf
angube <daniel.boassen@gmail.com>
2015-04-13 10:50:29 +0200

    Minor edits

--- WindowsFormsApplication9/TournamentTree.cs
+++ WindowsFormsApplication9/TournamentTree.cs
@@ -17,23 +17,30 @@ namespace GraphicTest
             InitializeComponent();
         }
 
-        //Code that creates lines
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            //Code that creates lines
             System.Drawing.Graphics graphicsObj;
             graphicsObj = this.CreateGraphics();
             Pen myPen = new Pen(System.Drawing.Color.Red, 3);
-            //Upper line             (x, y) start, (x, y) end
-            graphicsObj.DrawLine(myPen, 20, 40, 170, 40);
+            //Upper line             
+            graphicsObj.DrawLine(myPen,
+              //(x, y) start, (x, y) end
+                20, 40,      170, 40);
             //Lower line
-            graphicsObj.DrawLine(myPen, 20, 180, 170, 180);
+            graphicsObj.DrawLine(myPen, 
+                20, 180, 170, 180);
             //Upper to mid line
-            graphicsObj.DrawLine(myPen, 170, 40, 250, 110);
+            graphicsObj.DrawLine(myPen, 
+                170, 40, 250, 110);
             //Lower to mid line
-            graphicsObj.DrawLine(myPen, 170, 180, 250, 110);
+            graphicsObj.DrawLine(myPen, 
+                170, 180, 250, 110);
             //End line
-            graphicsObj.DrawLine(myPen, 250, 110, 280, 110);
+            graphicsObj.DrawLine(myPen, 
+                250, 110, 280, 110);
 
-        }        
+        }   
+          
     }
 }
